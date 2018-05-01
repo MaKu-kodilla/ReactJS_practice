38068742b41e1e9ff3375b26e51f6214841cd57a
Maciej Kubas <maciekkubas@gmail.com>
2018-05-01 14:34:07 +0200

    questions

---
+++ Zadanie 14.1 Zadanie istniejącej aplikacji/script.js
@@ -0,0 +1,44 @@
+var movies = [
+    {
+        id: 1,
+        title: 'Harry Potter',
+        desc: 'Film o czarodzieju',
+        image: 'https://www.rottentomatoes.com/franchise/harry_potter/'
+    },
+    {
+        id: 2,
+        title: 'Król Lew',
+        desc: 'Film o królu sawanny'
+    },
+    {
+        id: 3,
+        title: 'Władca Pierścieni',
+        desc: 'Film o podróży przez Śródziemie'
+    },
+    {
+        id: 4,
+        title: 'Matrix',
+        desc: 'Film o alternatywnej rzeczywistości'
+    },
+    {
+        id: 5, 
+        title: 'Avengers',
+        desc: 'Film o superbohaterach Marvela'
+    }
+];
+
+var moviesElements = movies.map(function(movie) {
+    return React.createElement('li', {key: movie.id},
+        React.createElement('h2', {}, movie.title),
+        React.createElement('p', {}, movie.desc),
+        React.createElement('img', {}, movie.image)
+    );
+});
+
+var element =  
+    React.createElement('div', {},
+        React.createElement('h1', {}, 'Lista Filmów'),
+        React.createElement('ul', {}, moviesElements),
+);
+
+ReactDOM.render(element, document.getElementById('app'));
